dfb216719fcc173edc6e7a52cb59bac83bc37fdf
Jaime Gil de Sagredo Luna <jgil@alea-soluciones.com>
2016-09-30 09:04:43 +0200

    Given a number divisible by 5 I get "Buzz"

--- fizzbuzz_spec.py
+++ fizzbuzz_spec.py
@@ -18,6 +18,10 @@ with describe('The Fizzbuzz game'):
         with it('returns "Buzz"'):
             expect(fizzbuzz(5)).to(equal('Buzz'))
 
+    with context('when the given number is divisible by 5'):
+        with it('returns "Buzz"'):
+            expect(fizzbuzz(10)).to(equal('Buzz'))
+
 
 
 # Devuelve Buzz si el número es divisible por 5.
