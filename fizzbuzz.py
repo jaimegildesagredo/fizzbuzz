3f4dbf4e1ff400528f1a95116b816f448c87977c
Jaime Gil de Sagredo Luna <jgil@alea-soluciones.com>
2016-09-30 09:00:45 +0200

    First test. Given 3 I get "Fizz"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 
-def fizzbuzz():
-    pass
+def fizzbuzz(number):
+    return 'Fizz'
--- fizzbuzz_spec.py
+++ fizzbuzz_spec.py
@@ -5,7 +5,13 @@ from expects import *
 from fizzbuzz import fizzbuzz
 
 
-with describe('Fizzbuzz'):
-    with context('when running the test'):
-        with it('says hello'):
-            expect('hello').to(equal('hello'))
+with describe('The Fizzbuzz game'):
+    with context('when the given number is 3'):
+        with it('returns "Fizz"'):
+            expect(fizzbuzz(3)).to(equal('Fizz'))
+
+
+
+# Devuelve Fizz si el número es divisible por 3.
+# Devuelve Buzz si el número es divisible por 5.
+# Devuelve FizzBuzz si el número es divisible por 3 y por 5.
